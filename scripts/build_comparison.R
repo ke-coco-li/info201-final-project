c483bc5bd622d42d8817f07c0dcd7f8194724e9c
alexkim0517 <ahk0517@uw.edu>
2019-03-11 17:22:16 -0700

    working on part

--- scripts/build_comparison.R
+++ scripts/build_comparison.R
@@ -6,40 +6,35 @@ dataset_sales <- read.csv("~/Desktop/info201-final-project/data/Sale_Prices_Stat
                           stringsAsFactors = F)
 dataset_rent <- read.csv("~/Desktop/info201-final-project/data/State_MedianRentalPrice_Sfr.csv", 
                          stringsAsFactors = F)
+#rent datset
 dataset_rent <- dataset_rent %>% select(1, 15:110)
-colnames(dataset_rent)[2:97] <- paste0('rent_', colnames(dataset_rent)[2:97])
 names(dataset_rent) = sub("X", "", names(dataset_rent))
 dataset_rent[is.na(dataset_rent)] <- 0
-
-
-
+#sales dataset
 dataset_sales <- dataset_sales %>% select(2, 38:133)
-colnames(dataset_sales)[2:97] <- paste0('sales_', colnames(dataset_sales)[2:97])
 names(dataset_sales) = sub("X", "", names(dataset_sales))
 dataset_sales[is.na(dataset_sales)] <- 0
 
 
-newset <- filter(dataset_rent, RegionName == "Texas") %>%
-  mutate("2011" = sum(newset[2:13]), 
-          "2012" = sum(newset[14:25])/12,
-          "2013" = sum(newset[26:37])/12,
-          "2014" = sum(newset[38:49])/12,
-          "2015" = sum(newset[50:61])/12,
-          "2016" = sum(newset[62:73])/12,
-          "2017" = sum(newset[74:85])/12,
-          "2018" = sum(newset[86:97])/12)
-newset <- select(newset, "RegionName", input$year)
+rentset <- filter(dataset_rent, RegionName == "input$state")
+rentset <- mutate(rentset, "rent_2011" = sum(rentset[2:13]/12), 
+          "rent_2012" = sum(rentset[14:25])/12,
+          "rent_2013" = sum(rentset[26:37])/12,
+          "rent_2014" = sum(rentset[38:49])/12,
+          "rent_2015" = sum(rentset[50:61])/12,
+          "rent_2016" = sum(rentset[62:73])/12,
+          "rent_2017" = sum(rentset[74:85])/12,
+          "rent_2018" = sum(rentset[86:97])/12)
+rentset <- select(rentset, "RegionName", "input$year")
 
-saleset <- filter(dataset_sales, RegionName == "Texas") %>%
-  mutate("2011" = sum(saleset[2:13])/12, 
-         "2012" = sum(saleset[14:25])/12,
-         "2013" = sum(saleset[26:37])/12,
-         "2014" = sum(saleset[38:49])/12,
-         "2015" = sum(saleset[50:61])/12,
-         "2016" = sum(saleset[62:73])/12,
-         "2017" = sum(saleset[74:85])/12,
-         "2018" = sum(saleset[86:97])/12)
-saleset <- select(saleset, "RegionName", input$year)
-
-
-combined <- merge(dataset_sales, dataset_rent, by = c("RegionName"))
+salesset <- filter(dataset_sales, RegionName == "Texas")
+salesset <- mutate(salesset, "sales_2011" = sum(salesset[2:13])/12, 
+                   "sales_2012" = sum(salesset[14:25])/12,
+                   "sales_2013" = sum(salesset[26:37])/12,
+                   "sales_2014" = sum(salesset[38:49])/12,
+                   "sales_2015" = sum(salesset[50:61])/12,
+                   "sales_2016" = sum(salesset[62:73])/12,
+                   "sales_2017" = sum(salesset[74:85])/12,
+                   "sales_2018" = sum(salesset[86:97])/12)
+salesset <- select(salesset, "RegionName", "2018")
+combined <- merge(rentset, salesset, by = c("RegionName"))
--- ui.R
+++ ui.R
@@ -1,98 +1,120 @@
 #Final Project: ui.R
 library(dplyr)
 library(shiny)
-library(shinythemes)
-library(plotly)
 library(leaflet)
+library(shinythemes)
 
 #Creating UI
 # Creating ui
-shinyUI(
-  navbarPage(
-    "Rent Statistics in the United States",
-    fluid = TRUE,
-    theme = shinytheme("superhero"),
+ui <- shinyUI(navbarPage(
+  "Rent Statistics in the United States",
+  fluid = TRUE, theme = shinytheme("superhero"),
+  
+  # tab panel #1:
+  tabPanel(
+    "Example #1",
+    titlePanel("Example Title")
+  ),
+  
+  # tab panel #2: rent affordability map
+  tabPanel(
+    "Rent Affordability Map",
+    titlePanel("2018 Rent Affordability Statistics"),
     
-    # tab panel #1:
-    tabPanel(
-      "Example #1",
-      titlePanel("Example Title")
-    ),
-    
-    # tab panel #2: rent affordability map
-    tabPanel(
-      "Rent Affordability Map",
-      titlePanel("2018 Rent Affordability Statistics"),
+    # Creating a sidbar layout
+    sidebarLayout(
       
-      # Creating a sidbar layout
-      sidebarLayout(
-        # side panel for controls
-        sidebarPanel(
-          # 'radioButtons' to change the # of cities shown
-          radioButtons(
-            "city_size",
-            label = "City Size Rank",
-            choices = list(
-              "Top 10" = 10,
-              "Top 50" = 50,
-              "Top 100" = 100,
-              "Top 300" = 600
-            ),
-            selected = 50
+      # side panel for controls
+      sidebarPanel(
+        
+        # 'radioButtons' to change the # of cities shown
+        radioButtons(
+          "city_size",
+          label = "City Size Rank",
+          choices = list(
+            "Top 10" = 10, "Top 50" = 50,
+            "Top 100" = 100, "Top 300" = 600
           ),
-          
-          # 'sliderInput' to change the affordability % threshold - NEED TO EDIT
-          sliderInput(
-            "percentage",
-            label = "Affordability Percentage",
-            min = .1,
-            max = .5,
-            value = .30
-          )
+          selected = 50
         ),
         
-        # Main panel for displaying rent affordability map
-        mainPanel(leafletOutput("affordability_map"))
-      )
+        # 'sliderInput' to change the affordability % threshold - NEED TO EDIT
+        sliderInput(
+          "percentage",
+          label = "Affordability Percentage",
+          min = .1,
+          max = .5,
+          value = .30
+        )),
       
-    ),
-    # tab panel #3:
-    tabPanel("Example #2",
-             titlePanel("Example Title")),
+      # Main panel for displaying rent affordability map
+      mainPanel(
+        leafletOutput("affordability_map")
+      )
+    )
     
-    # tab panel #4:
-    tabPanel("Example #3",
-             titlePanel("Example Title")),
-    # tab panel #5:
-    tabPanel(
-      "Rent vs. Sales",
-      titlePanel("Side by Side Comparison of Rent and Sales"),
-      sidebarLayout(
-        sidebarPanel(
-          textInput("state",
-                    label = "Type in A State of Interest",
-                    placeholder = "Enter full State name (eg. Texas)"),
-          selectInput(
-            "year",
-            label = "Year",
-            choices = c("2011",
-                        "2012",
-                        "2013",
-                        "2014",
-                        "2015",
-                        "2016",
-                        "2017",
-                        "2018")
-          )
+  ),
+  # tab panel #3:
+  tabPanel(
+    "Example #2",
+    titlePanel("Example Title")
+  ),
+  
+  # tab panel #4:
+  tabPanel(
+    "Rent Trend Plot",
+    titlePanel("Median Monthly Rent from 2010 to 2019"),
+    sidebarLayout(
+      sidebarPanel(
+        textInput(
+          "chosen_state", 
+          label = "Type in A State of Your Interest", 
+          value = "United States", 
+          placeholder = "Enter the abbreviation (eg. NY)"
         ),
-        mainPanel(
-          plotlyOutput("race_plot"),
-          p(
-            "*Data for some years was unavailable from the source which resulted in the 0's
-            and low outliers shown in the data."
-          )
-          )
+        uiOutput("select_city"),
+        checkboxGroupInput("home_types",
+                           "Please select home types:",
+                           c("All Homes","Studio","One Bedroom","Two Bedrooms","Three Bedrooms",
+                             "Four Bedrooms","Five Bedrooms or More","Condo And Co-op",
+                             "Duplex And Triplex","Single Family Residence","Multi-family Residence (5+)"),
+                           selected = "All Homes")
+      ),
+      mainPanel(
+        plotOutput("plot")
+      )
+    )
+  ),
+  # tab panel #5:
+  tabPanel(
+    "Rent vs. Sales",
+    titlePanel("Side by Side Comparison of Rent and Sales"),
+    sidebarLayout(
+      sidebarPanel(
+        textInput("state",
+                  label = "Type in A State of Interest",
+                  placeholder = "Enter full State name (eg. Texas)"),
+        selectInput(
+          "year",
+          label = "Year",
+          choices = c("2011",
+                      "2012",
+                      "2013",
+                      "2014",
+                      "2015",
+                      "2016",
+                      "2017",
+                      "2018")
+        )
+      ),
+      mainPanel(
+        plotlyOutput("rentvsalesplot"),
+        p(
+          "*Data for some years was unavailable from the source which resulted in the 0's
+          and low outliers shown in the data."
         )
       )
     )
   )
+)
+)
