7ffd64caa3b3859948cb0bfd48e93281bb2dc3a7
Jameson McFarland <mcfarj2@uw.edu>
2019-03-13 12:17:42 -0700

    reordered tabs

--- ui.R
+++ ui.R
@@ -61,47 +61,6 @@ ui <- shinyUI(navbarPage(
   
   # tab panel #3:
   tabPanel(
-    "City Finder Tool",
-    titlePanel("Find Cities That Match Your Budget"),
-    sidebarLayout(
-      sidebarPanel(
-        numericInput("monthly_income",
-                     label = "Monthly Income",
-                     value = 4000),
-        sliderInput("percent_income",
-                    label = "Percentage of Monthly Income for Rent",
-                    min = 0,
-                    max = 100,
-                    value = 25),
-        radioButtons("rental_type",
-                     label = "Rental Type",
-                     choices = list("All Homes",
-                                 "Studio",
-                                 "One Bedroom",
-                                 "Two Bedrooms",
-                                 "Three Bedrooms",
-                                 "Four Bedrooms",
-                                 "Five Bedrooms or More",
-                                 "Condo and Co-op",
-                                 "Duplex And Triplex",
-                                 "Single Family Residence",
-                                 "Multi-family Residence (5+)"),
-                     selected = "All Homes")
-      ),
-      mainPanel(
-        p(textOutput("budget")),
-        h3("Cities Closest to Your Rent Budget"),
-        p("The following fifteen cities are recommended
-          based on your specified budget and rental type.
-          These cities are recommended based on how closely median
-          rental prices in that area match your budget."),
-        tableOutput("city_list")
-      )
-    )
-  ),
-  
-  # tab panel #4:
-  tabPanel(
     "Rent Trend Plot",
     titlePanel("Median Monthly Rent 2010-2019"),
     p("As shown in the default plot, from 2010 to 2019, the median monthly rate within 
@@ -128,46 +87,88 @@ ui <- shinyUI(navbarPage(
       mainPanel(
         plotOutput("trend_plot")
       )
-    )
+      )
     ),
-    # tab panel #5:
-    tabPanel(
-      "Rent vs. Sales",
-      titlePanel("Side by Side Comparison of Rent and Sales"),
-      sidebarLayout(
-        sidebarPanel(
-          textInput(
-            "state",
-            label = "Type In The State of Interest",
-            value = "Washington",
-            placeholder = "Enter full State name (eg. Texas)"
-          ),
-          selectInput(
-            "year",
-            label = "Select a Year to Calculate",
-            choices = list("2012",
-                           "2013",
-                           "2014",
-                           "2015",
-                           "2016",
-                           "2017",
-                           "2018")
-          )
+  
+  # tab panel #4:
+  tabPanel(
+    "Rent vs. Sales",
+    titlePanel("Side by Side Comparison of Rent and Sales"),
+    sidebarLayout(
+      sidebarPanel(
+        textInput(
+          "state",
+          label = "Type In The State of Interest",
+          value = "Washington",
+          placeholder = "Enter full State name (eg. Texas)"
         ),
-        mainPanel(
-          plotOutput("rentplot", width = "100%", height = "270px"),
-          br(),
-          plotOutput("salesplot", width = "100%", height = "270px"),
-          em("*Data for some years was unavailable from the source which resulted in the 0's
-            and low outliers shown in the data."),
-          br(),
-          br(),
-          p("These two plots inform viewers of the trends of the price of house sales 
-            and rental price of homes over the years. Users can also select a certain year to see
-            how many months it would have taken to pay for the average house with the average rent cost. 
-            It would have taken approximately", textOutput("month"), "months to buy a house with the 
-            amount of rent being paid.")
-          )
+        selectInput(
+          "year",
+          label = "Select a Year to Calculate",
+          choices = list("2012",
+                         "2013",
+                         "2014",
+                         "2015",
+                         "2016",
+                         "2017",
+                         "2018")
         )
+      ),
+      mainPanel(
+        plotOutput("rentplot", width = "100%", height = "270px"),
+        br(),
+        plotOutput("salesplot", width = "100%", height = "270px"),
+        em("*Data for some years was unavailable from the source which resulted in the 0's
+           and low outliers shown in the data."),
+        br(),
+        br(),
+        p("These two plots inform viewers of the trends of the price of house sales 
+          and rental price of homes over the years. Users can also select a certain year to see
+          how many months it would have taken to pay for the average house with the average rent cost. 
+          It would have taken approximately", textOutput("month"), "months to buy a house with the 
+          amount of rent being paid.")
+        )
+        )
+        ),
+  
+    # tab panel #5:
+  tabPanel(
+    "City Finder Tool",
+    titlePanel("Find Cities That Match Your Budget"),
+    sidebarLayout(
+      sidebarPanel(
+        numericInput("monthly_income",
+                     label = "Monthly Income",
+                     value = 4000),
+        sliderInput("percent_income",
+                    label = "Percentage of Monthly Income for Rent",
+                    min = 0,
+                    max = 100,
+                    value = 25),
+        radioButtons("rental_type",
+                     label = "Rental Type",
+                     choices = list("All Homes",
+                                    "Studio",
+                                    "One Bedroom",
+                                    "Two Bedrooms",
+                                    "Three Bedrooms",
+                                    "Four Bedrooms",
+                                    "Five Bedrooms or More",
+                                    "Condo and Co-op",
+                                    "Duplex And Triplex",
+                                    "Single Family Residence",
+                                    "Multi-family Residence (5+)"),
+                     selected = "All Homes")
+      ),
+      mainPanel(
+        p(textOutput("budget")),
+        h3("Cities Closest to Your Rent Budget"),
+        p("The following fifteen cities are recommended
+          based on your specified budget and rental type.
+          These cities are recommended based on how closely median
+          rental prices in that area match your budget."),
+        tableOutput("city_list")
+        )
+      )
       )
 ))
