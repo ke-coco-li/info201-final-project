531af6ef3d5a3f1380864a146253df3eed49da74
Jameson McFarland <mcfarj2@uw.edu>
2019-03-14 12:32:34 -0700

    formatting tab 5

--- ui.R
+++ ui.R
@@ -184,7 +184,7 @@ ui <- shinyUI(navbarPage(
       ),
       mainPanel(
         p(),
-        h3(textOutput("budget")),
+        h2(textOutput("budget")),
         p(),
         h3("Cities Closest to Your Rent Budget"),
         p("The following fifteen cities are recommended
