0d05345c2956c0fc328ecf934b4354d195c3669a
Coco Li <coco@D-10-19-208-159.dhcp4.washington.edu>
2019-03-14 14:55:33 -0700

    lint

--- server.R
+++ server.R
@@ -14,6 +14,18 @@ source("./scripts/build_comparison.R")
 
 afford_data <- read.csv("data/Affordability_Wide_lat_long_rent_only.csv",
                         stringsAsFactors = F)
+
+all_states <- c("Alabama", "Alaska", "Arizona", "Arkansas", "California", 
+                "Colorado", "Connecticut", "DC", "Delaware", "Florida", 
+                "Georgia", "Hawaii", "Idaho","Illinois", "Indiana", "Iowa", 
+                "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", 
+                "Massachuesetts", "Michigan", "Minnesota", "Mississippi", 
+                "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", 
+                "New Jersey", "New Mexico", "New York", "North Carolina", 
+                "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", 
+                "Rhode Island", "South Carolina", "South Dakota", 
+                "Tennessee", "Texas", "Utah", "Vermont", "Virginia", 
+                "Washington", "West Virginia", "Wisconsin","Wyoming")
 #income_data <- read.csv("data/Affordability_Wide_lat_long_rent_only.csv",
  #                       stringsAsFactors = F)
 #join data
@@ -39,10 +51,15 @@ server <- shinyServer(function(input, output) {
   
   chosen_state <- reactive({
     validate(
-      need(input$chosen_state %in% c("AL","AK","AZ","AR","CA","CO","CT","DC","DE","FL","GA","HI","ID",
-                                    "IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO",
-                                    "MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA",
-                                    "RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
+      need(input$chosen_state %in% c("AL", "AK", "AZ", "AR", "CA", "CO",
+                                     "CT", "DC", "DE", "FL", "GA", "HI",
+                                     "ID", "IL", "IN", "IA", "KS", "KY", 
+                                     "LA", "ME", "MD", "MA", "MI", "MN", 
+                                     "MS", "MO", "MT", "NE", "NV", "NH", 
+                                     "NJ", "NM", "NY", "NC", "ND", "OH", 
+                                     "OK", "OR", "PA", "RI", "SC", "SD", 
+                                     "TN", "TX", "UT", "VT", "VA", "WA",
+                                     "WV", "WI", "WY")
            ,"Please enter an eligible abbreviation.")
     )
     input$chosen_state
@@ -128,8 +145,10 @@ server <- shinyServer(function(input, output) {
     add_line <- function(data, colname) {
       if (nrow(data) != 0) {
         data2 <- melt(data, id = "RegionName")
-        data2$variable <- as.Date(paste0(data2$variable,"-01"), format = "%Y-%m-%d")
-        geom_line(data = data2, aes(x = variable, y = value, col = colname, group = 1))
+        data2$variable <- as.Date(paste0(data2$variable,"-01"), 
+                                  format = "%Y-%m-%d")
+        geom_line(data = data2, aes(x = variable, y = value, 
+                                    col = colname, group = 1))
       }
     }
     p <- ggplot()
@@ -177,78 +196,76 @@ server <- shinyServer(function(input, output) {
       home_data <- reformat_colnames(mfr)
       p <- p + add_line(home_data, "MFR")
     }
-    p + labs(x = "year", y = "median monthly rent", title = "Monthly Rent vs. Time", colour = "Home types")
+    p + labs(x = "year", y = "median monthly rent", 
+             title = "Monthly Rent vs. Time", colour = "Home types")
   })
   
+  
   output$rentplot <- renderPlot({
-    rentset <- filter(dataset_rent, RegionName == chosen_state_full())
-    rentset <- mutate(rentset, "2011" = round(sum(rentset[2:13])/12), 
-                      "2012" = round(sum(rentset[14:25])/12),
-                      "2013" = round(sum(rentset[26:37])/12),
-                      "2014" = round(sum(rentset[38:49])/12),
-                      "2015" = round(sum(rentset[50:61])/12),
-                      "2016" = round(sum(rentset[62:73])/12),
-                      "2017" = round(sum(rentset[74:85])/12),
-                      "2018" = round(sum(rentset[86:97])/12))
-    rentset <- select(rentset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
-    return(plot(x= colnames(rentset), y= rentset, type = "b", col = "blue", 
-                xlab = "Year", ylab = "Price in $", main = "Average Monthly Rent Price of Homes") +
-             text(x= colnames(rentset), y = rentset[1:7], paste0("$", rentset), pos = 3) +
-             text(x= colnames(rentset[8]), y = rentset[8], paste0("$", rentset$`2018`), pos = 1))
+    if (input$chosen_state_full %in% all_states) {
+      rentset <- filter(dataset_rent, RegionName == input$chosen_state_full)
+      rentset <- mutate(rentset, "2011" = round(sum(rentset[2:13])/12), 
+                        "2012" = round(sum(rentset[14:25])/12),
+                        "2013" = round(sum(rentset[26:37])/12),
+                        "2014" = round(sum(rentset[38:49])/12),
+                        "2015" = round(sum(rentset[50:61])/12),
+                        "2016" = round(sum(rentset[62:73])/12),
+                        "2017" = round(sum(rentset[74:85])/12),
+                        "2018" = round(sum(rentset[86:97])/12))
+      rentset <- select(rentset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
+      return(plot(x= colnames(rentset), y= rentset, type = "b", col = "blue", 
+                  xlab = "Year", ylab = "Price in $", main = "Average Monthly Rent Price of Homes") +
+               text(x= colnames(rentset), y = rentset[1:7], paste0("$", rentset), pos = 3) +
+               text(x= colnames(rentset[8]), y = rentset[8], paste0("$", rentset$`2018`), pos = 1))
+    } else {
+      return (NULL)
+    }
   })
   
   output$salesplot <- renderPlot({
-    salesset <- filter(dataset_sales, RegionName == chosen_state_full())
-    salesset <- mutate(salesset, "2011" = round(sum(salesset[2:13])/12), 
-                       "2012" = round(sum(salesset[14:25])/12),
-                       "2013" = round(sum(salesset[26:37])/12),
-                       "2014" = round(sum(salesset[38:49])/12),
-                       "2015" = round(sum(salesset[50:61])/12),
-                       "2016" = round(sum(salesset[62:73])/12),
-                       "2017" = round(sum(salesset[74:85])/12),
-                       "2018" = round(sum(salesset[86:97])/12))
-    salesset <- select(salesset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
-    return(plot(x= colnames(salesset), y= salesset, type = "b", col = "red",
-                xlab = "Year", ylab = "Price in $", main = "Average Sale Price of Homes")+
-             text(x= colnames(salesset), y = salesset[1:7], paste0("$", salesset), pos = 4) +
-             text(x= colnames(salesset[8]), y = salesset[8], paste0("$", salesset$`2018`), pos = 2))
+    if (input$chosen_state_full %in% all_states) {
+      salesset <- filter(dataset_sales, RegionName == input$chosen_state_full)
+      salesset <- mutate(salesset, "2011" = round(sum(salesset[2:13])/12), 
+                         "2012" = round(sum(salesset[14:25])/12),
+                         "2013" = round(sum(salesset[26:37])/12),
+                         "2014" = round(sum(salesset[38:49])/12),
+                         "2015" = round(sum(salesset[50:61])/12),
+                         "2016" = round(sum(salesset[62:73])/12),
+                         "2017" = round(sum(salesset[74:85])/12),
+                         "2018" = round(sum(salesset[86:97])/12))
+      salesset <- select(salesset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
+      return(plot(x= colnames(salesset), y= salesset, type = "b", col = "red",
+                  xlab = "Year", ylab = "Price in $", main = "Average Sale Price of Homes")+
+               text(x= colnames(salesset), y = salesset[1:7], paste0("$", salesset), pos = 4) +
+               text(x= colnames(salesset[8]), y = salesset[8], paste0("$", salesset$`2018`), pos = 2))
+    }
   })
   
   output$month <- renderText({
-    salesset <- filter(dataset_sales, RegionName == chosen_state_full())
-    salesset <- mutate(salesset, "2011" = round(sum(salesset[2:13])/12), 
-                       "2012" = round(sum(salesset[14:25])/12),
-                       "2013" = round(sum(salesset[26:37])/12),
-                       "2014" = round(sum(salesset[38:49])/12),
-                       "2015" = round(sum(salesset[50:61])/12),
-                       "2016" = round(sum(salesset[62:73])/12),
-                       "2017" = round(sum(salesset[74:85])/12),
-                       "2018" = round(sum(salesset[86:97])/12))
-    salesset <- select(salesset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
-    rentset <- filter(dataset_rent, RegionName == chosen_state_full())
-    rentset <- mutate(rentset, "2011" = round(sum(rentset[2:13])/12), 
-                      "2012" = round(sum(rentset[14:25])/12),
-                      "2013" = round(sum(rentset[26:37])/12),
-                      "2014" = round(sum(rentset[38:49])/12),
-                      "2015" = round(sum(rentset[50:61])/12),
-                      "2016" = round(sum(rentset[62:73])/12),
-                      "2017" = round(sum(rentset[74:85])/12),
-                      "2018" = round(sum(rentset[86:97])/12))
-    rentset <- select(rentset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
-    full_text <- paste0("It would have taken approximately ", round(salesset[input$year]/rentset[input$year]), 
-                        " months to buy a house with the amount of rent being paid.")
+    if (input$chosen_state_full %in% all_states) {
+      salesset <- filter(dataset_sales, RegionName == input$chosen_state_full)
+      salesset <- mutate(salesset, "2011" = round(sum(salesset[2:13])/12), 
+                         "2012" = round(sum(salesset[14:25])/12),
+                         "2013" = round(sum(salesset[26:37])/12),
+                         "2014" = round(sum(salesset[38:49])/12),
+                         "2015" = round(sum(salesset[50:61])/12),
+                         "2016" = round(sum(salesset[62:73])/12),
+                         "2017" = round(sum(salesset[74:85])/12),
+                         "2018" = round(sum(salesset[86:97])/12))
+      salesset <- select(salesset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
+      rentset <- filter(dataset_rent, RegionName == input$chosen_state_full)
+      rentset <- mutate(rentset, "2011" = round(sum(rentset[2:13])/12), 
+                        "2012" = round(sum(rentset[14:25])/12),
+                        "2013" = round(sum(rentset[26:37])/12),
+                        "2014" = round(sum(rentset[38:49])/12),
+                        "2015" = round(sum(rentset[50:61])/12),
+                        "2016" = round(sum(rentset[62:73])/12),
+                        "2017" = round(sum(rentset[74:85])/12),
+                        "2018" = round(sum(rentset[86:97])/12))
+      rentset <- select(rentset, "RegionName", "2012", "2013", "2014", "2015", "2016", "2017", "2018")
+      full_text <- paste0("It would have taken approximately ", round(salesset[input$year]/rentset[input$year]), 
+                          " months to buy a house with the amount of rent being paid.")
+    }
   })
-  chosen_state_full <- reactive({
-    validate(need(input$chosen_state_full %in% c("Alabama","Alaska","Arizona","Arkansas","California","Colorado",
-                                          "Connecticut","DC","Delaware","Florida","Georgia","Hawaii","Idaho",
-                                     "Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine",
-                                     "Maryland","Massachuesetts","Michigan","Minnesota","Mississippi","Missouri",
-                                     "Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
-                                     "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
-                                     "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
-                                     "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming")
-           ,"Please enter an eligible state")
-    )
-    input$chosen_state_full
-  })
-  })
+  
+})
